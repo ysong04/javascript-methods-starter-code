9162b69aee86a32a537fc4fde83609c06acd1df6
Yoomin Song <yoominsong@Yoomins-MacBook-Air.local>
2022-10-07 22:46:03 -0400

    updated //FILTER// and //REDUCE//

--- javascript-methods.js
+++ javascript-methods.js
@@ -26,23 +26,25 @@ Array.prototype.myMap = function (cbFunc) {
 };
 
 // FILTER //
-Array.prototype.myFilter = function(callbackFn) {
-  let result = [];
+Array.prototype.myFilter = function(callbackFn, thisArg) {
+  let array = this;// take in an array of elements 
+  let result_array = []; 
 
-  for (let i = 0  ; i < this.length ; i++){
-    if (this[i] === undefined)
-    {
-      continue;    // If an element in the array is undefined, skip it
+  for(let i = 0; i < array.length; i++) {
+    let callback_arguments = [array[i], i, array]; //callback_arguments 
+
+    if(thisArg) {
+      if(callbackFn.call(thisArg, ...callback_arguments)) { //callback function on each element 
+        result_array.push(array[i]);
+      };
     }
-
-    if( cbFunc(this[i], i, this))
-    {
-        push(result, this[i]);
-        // We use the push method here instead of saving the value at the same Index
-        // it was placed in the input array so that there are no empty slots in the result array
+    else {
+      if(callbackFn(...callback_arguments)) { //callback function on each element 
+        result_array.push(array[i]);
+      };
     }
   }
-  return result;
+  return result_array; // return resulting array
 };
 
 // SOME //
@@ -86,9 +88,27 @@ Array.prototype.myEvery = function(callbackFn) {
 };
 
 // REDUCE //
-Array.prototype.myReduce = function(callbackFn) {
-  // Place your code here.
-};
+Array.prototype.myReduce = function(callbackFn, initialValue) {
+
+  // if no initial value is given or array is empty, throw error.
+  if (initialValue === undefined && !this.length) {
+    throw new TypeError("Incorrect.")
+  }
+
+  let total = initialValue; 
+  let current_index = 0; 
+
+  // if initialValue is undefined, update total and current_index 
+  if(initialValue == undefined) { 
+    total = this[0];
+    current_index = 1; 
+  }
+
+  for(let i = current_index; i < this.length; i++) {
+    total = callbackFn(total, this[i], i, this);
+  }
+  return total;
+};// Place your code here.
 
 // INCLUDES //
 Array.prototype.myIncludes = function(searchElement) {
